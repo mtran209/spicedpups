6e909692db328f3e82412335aee39d659ade281c
Minh Tran <mtran209@hotmail.com>
2019-11-03 19:23:53 -0800

    Adding animations, more javascript, and hidden modals

--- js/scripts.js
+++ js/scripts.js
@@ -13,11 +13,29 @@ function animateCSS(element, animationName, callback) {
 }
 
 $(function() {
+ // Modal Triggering   
+    
     $("#reserveButton").click(function(){
         $("#reserveModal").modal("show");
     });
     $("#scheduleLink").click(function(){
         $("#reserveModal").modal("show");
+    });
+    $("#rosieTrig").click(function(){
+        $("#rosieModal").modal("show");
+    });
+    $("#basilandcinnTrig").click(function(){
+        $("#basilandcinnModal").modal("show");
+    });
+
+// Animations/Toggls
+
+    $('#cinnamonPic').click(function() {
+        if ($('#cinnamonPic').attr('src', 'img/cinnamonaccord.jpg')) {
+            $('#cinnamonPic').attr('src', 'img/IMG_5935.jpeg');
+        }   else {
+            $('#cinnamonPic').attr('src', 'img/cinnamonaccord.jpg');
+        }
     });
 });
 
